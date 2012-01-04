0831465bcff96a60e59ea398b6c2f8589a6c31dc
Uros Marolt <bizzyizdizzy@gmail.com>
2012-01-04 19:35:30 +0100

    Added frame (also testing push configuration)

--- AnimationFW/src/info/bizzyizdizzy/animationframework/lwjgl/samples/GameloopExample.java
+++ AnimationFW/src/info/bizzyizdizzy/animationframework/lwjgl/samples/GameloopExample.java
@@ -1,5 +1,13 @@
 package info.bizzyizdizzy.animationframework.lwjgl.samples;
 
 public class GameloopExample {
-
+	
+	public void start(){
+		
+	}
+	
+	public static void main(String[] args){
+		GameloopExample gameloopExample = new GameloopExample();
+		gameloopExample.start();
+	}
 }
